216e2b72cd50c4bb9f125950cb4625230e6c6e72
Daniele Rossi <reds6188@gmail.com>
2024-07-15 11:38:24 +0200

    Added library "esp-console"

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "main.h"
 
-Timer TimerBlink1, TimerBlink2;
+Timer TimerBlink2, TimerText;
 Uart uart_232(UART_232);
 Led Led1(LED_1);
 Led Led2(LED_2);
+Led Led3(LED_3, true);
 
+uint8_t text_counter;
 const char * str = "Hello World!";
 
 uart_pin_t UART_232_pin {
@@ -28,15 +30,23 @@ void uart_rx_callback (uint8_t *data, int data_size) {
 	digitalWrite(LED_3, !digitalRead(LED_3));
 }
 
+void setLed3(void) {
+	Led3.set();
+}
+
+void resetLed3(void) {
+	Led3.reset();
+}
+
 void setup() {
-	Serial.begin(115200);
-	TimerBlink1.trigger();
+	console.header(DOUBLE_DASHED, LOG_BLUE, 80, "START INITIALIZATION");
 	Led1.startBlink(100,900);
-	pinMode(LED_3, OUTPUT);
+	Led3.reset();
 	pinMode(UART_232_EN, OUTPUT);
 	digitalWrite(UART_232_EN, HIGH);
 	uart_232.begin(UART_232_pin, UART_232_config);
 	uart_232.setHandler(uart_rx_callback);
+	console.header(DASHED, LOG_GREN, 80, "END INITIALIZATION");
 }
 
 void loop() {
@@ -46,5 +56,22 @@ void loop() {
 		TimerBlink2.trigger();
 		Led2.toggle();
 		uart_232.writeData((uint8_t *)str, strlen(str));
+	}
+
+	if(TimerText.elapsedX1s(2)) {
+		TimerText.trigger();
+		if(text_counter < 4)
+			text_counter++;
+		else
+			text_counter = 0;
+
+		console.line(DOUBLE_DASHED, 80);
+		switch(text_counter) {
+			case 0: console.log(MAIN_T, "This text is normal");			break;
+			case 1: console.info(MAIN_T, "Information message");		break;
+			case 2: console.success(MAIN_T, "OK messagge");				break;
+			case 3: console.warning(MAIN_T, "Warning messagge");		break;
+			case 4: console.error(MAIN_T, "Error message");				break;
+		}
 	}
 }
--- src/main.h
+++ src/main.h
@@ -2,9 +2,13 @@
 #define MAIN_H_
 
 #include <Arduino.h>
+#include <console.h>
 #include <timer.h>
 #include <serial.h>
 #include <led.h>
 #include "def.h"
+#include "button.h"
+
+#define MAIN_T      "MAIN"      // tag for log
 
 #endif  /* MAIN_H_ */
